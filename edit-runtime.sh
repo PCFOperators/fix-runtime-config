a04006f1d15c912181794de1e5ae6202bcd627c6
David Lohle <Proplex@users.noreply.github.com>
2019-03-02 14:12:56 -0500

    Let's try something else

--- edit-runtime.sh
+++ edit-runtime.sh
@@ -14,7 +14,17 @@ addons:
     - os: ubuntu-xenial
   jobs:
   - name: node_exporter
-release: node-exporter
+    release: node-exporter
 EOT
 
-cp -R pcf-prometheus-pipeline fixed-runtime
+
+source pcf-prometheus-pipeline/pipeline/tasks/common.sh
+
+login_to_director pcf-bosh-creds
+
+echo "Uploading Node exporter Release..."
+bosh -n upload-release node-exporter-release/node-exporter-*.tgz
+
+node_exporter_version=$(cat node-exporter-release/version)
+bosh -n update-runtime-config --name=node_exporter pcf-prometheus-pipeline/runtime.yml -v node_exporter_version=${node_exporter_version}
+
